330f8e6bb804ae75e3fdd3265fcbcba90e6e5e8c
Jeka <admingeka@gmail.com>
2015-07-30 16:51:06 +0300

    Add open Customer Activity by notification

--- app/src/main/java/ru/veloportation/veloport/ui/activities/CustomerActivity.java
+++ app/src/main/java/ru/veloportation/veloport/ui/activities/CustomerActivity.java
@@ -12,27 +12,6 @@ public class CustomerActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-        //Intent intent = getIntent();
-        //int startType = intent.getIntExtra(START_TYPE, START_COURIER);
-
         addFragment(new OrdersMenuFragment(), false);
     }
-
-    /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_customer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    } */
 }
